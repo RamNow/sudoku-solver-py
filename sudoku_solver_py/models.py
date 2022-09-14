2f1a8481599dbeb53b8ab64561daff4ae9f11b41
Christian Nau <nau.forum@gmail.com>
2022-09-15 00:00:50 +0200

    feat: adds models + board initialization

---
+++ sudoku_solver_py/models.py
@@ -0,0 +1,94 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Protocol
+from pydantic import BaseModel, Field
+
+
+class Model(BaseModel, ABC):
+    @abstractmethod
+    def is_valid() -> bool:
+        """Checks whether or not all Sudoku rules are followed."""
+
+
+class Cell(Model):
+    value: int
+
+    def is_valid() -> bool:
+        pass
+
+class GroupModel(Model):
+    index: int
+    cells: list[Cell] = Field(default_factory=list)
+
+    def is_valid() -> bool:
+        pass
+
+    def __str__(self) -> str:
+        cell_values = [str(c.value) for c in self.cells]
+        return ''.join(cell_values)
+
+
+class Row(GroupModel):
+   pass
+
+
+class Column(GroupModel):
+    pass
+
+
+class Box(GroupModel):
+    pass 
+    
+    # Box index within a Grid, and cell index within a box are counting from left to right and from top to bottom: 
+    # 0 1 2
+    # 3 4 5
+    # 6 7 8
+   
+
+
+class Grid(Model):
+    """Representation of a Sudoku board."""
+    
+    cells: list[Cell] = Field(default_factory=list)
+    rows: list[Row] = Field(default_factory=list)
+    columns: list[Column] = Field(default_factory=list)
+    boxes: list[Box] = Field(default_factory=list)
+
+    def __init__(self, numbers: str) -> None:
+        super().__init__()
+
+        self.init_groups()
+
+        # removed unwanted chars
+        numbers = numbers.replace(',','').replace(' ', '')
+    
+        for row_index in range(9):
+
+            row_values = numbers[row_index*9:(row_index*9)+9]
+            
+            for col_index in range(9):
+                
+                val = row_values[col_index]
+                cell = Cell(value=int(val))
+                self.cells.append(cell)
+                self.rows[row_index].cells.append(cell)
+                self.columns[col_index].cells.append(cell)
+
+                # figure out to which box the cell belongs
+                box_index = self.find_box_index(row_index, col_index)
+                self.boxes[box_index].cells.append(cell)
+
+    @staticmethod
+    def find_box_index(row: int, col: int) -> int:
+        return row // 3 * 3 + col // 3
+
+
+    def init_groups(self):
+        for i in range(9):
+            self.rows.append(Row(index=i))
+            self.columns.append(Column(index=i))
+            self.boxes.append(Box(index=i))
+
+    def is_valid() -> bool:
+        pass
+            
---
+++ tests/test_models.py
@@ -0,0 +1,74 @@
+from sudoku_solver_py.models import Grid
+
+def test_find_box_index():
+    
+    assert Grid.find_box_index(0, 0) == 0
+    assert Grid.find_box_index(1, 2) == 0
+    assert Grid.find_box_index(2, 1) == 0
+    assert Grid.find_box_index(0, 3) == 1
+    assert Grid.find_box_index(0, 6) == 2
+    assert Grid.find_box_index(3, 0) == 3
+    assert Grid.find_box_index(4, 0) == 3
+    assert Grid.find_box_index(4, 2) == 3
+    assert Grid.find_box_index(3, 3) == 4
+    assert Grid.find_box_index(3, 6) == 5
+    assert Grid.find_box_index(6, 0) == 6
+    assert Grid.find_box_index(6, 3) == 7
+    assert Grid.find_box_index(6, 6) == 8
+    assert Grid.find_box_index(7, 7) == 8
+    assert Grid.find_box_index(8, 8) == 8
+
+
+def test_init_grid():
+
+    # arrange
+    numbers = '2,3,7 8,4,1 5,6,9 186795243 594326718 315674892 469582137 728139456 642918375 853467921 971253684'
+    # act
+    grid = Grid(numbers)
+    # assert
+    assert len(grid.cells) == 9*9
+    assert len(grid.rows) == 9
+    assert len(grid.columns) == 9
+    assert len(grid.boxes) == 9
+
+    numbers = numbers.replace(' ', '').replace(',', '')
+    
+    for i in range(9*9):
+        val = int(numbers[i])
+        assert grid.cells[i].value == val
+
+    assert grid.rows[0].cells[0].value == 2
+    assert grid.rows[0].cells[1].value == 3
+    assert grid.rows[0].cells[2].value == 7
+    assert grid.rows[0].cells[3].value == 8
+    assert grid.rows[1].cells[3].value == 7
+
+    assert str(grid.rows[0]) == '237841569'
+    assert str(grid.rows[1]) == '186795243'
+    assert str(grid.rows[2]) == '594326718'
+    assert str(grid.rows[3]) == '315674892'
+    assert str(grid.rows[4]) == '469582137'
+    assert str(grid.rows[5]) == '728139456'
+    assert str(grid.rows[6]) == '642918375'
+    assert str(grid.rows[7]) == '853467921'
+    assert str(grid.rows[8]) == '971253684'
+    
+    assert str(grid.columns[0]) == '215347689'
+    assert str(grid.columns[1]) == '389162457'
+    assert str(grid.columns[2]) == '764598231'
+    assert str(grid.columns[3]) == '873651942'
+    assert str(grid.columns[4]) == '492783165'
+    assert str(grid.columns[5]) == '156429873'
+    assert str(grid.columns[6]) == '527814396'
+    assert str(grid.columns[7]) == '641935728'
+    assert str(grid.columns[8]) == '938276514'
+
+    assert str(grid.boxes[0]) == '237186594'
+    assert str(grid.boxes[1]) == '841795326'
+    assert str(grid.boxes[2]) == '569243718'
+    assert str(grid.boxes[3]) == '315469728'
+    assert str(grid.boxes[4]) == '674582139'
+    assert str(grid.boxes[5]) == '892137456'
+    assert str(grid.boxes[6]) == '642853971'
+    assert str(grid.boxes[7]) == '918467253'
+    assert str(grid.boxes[8]) == '375921684'
