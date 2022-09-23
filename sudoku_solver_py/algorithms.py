589fc2e2002f04e1208b62bbf24aee64177b8165
Christian Nau <nau.forum@gmail.com>
2022-09-23 21:29:06 +0200

    feat: "naked triplet" algorithm

--- sudoku_solver_py/algorithms.py
+++ sudoku_solver_py/algorithms.py
@@ -41,7 +41,7 @@ def reduce_candidates_by_group_constraints(grid: Grid) -> bool:
     return some_candidates_reduced
     
 
-def remove_from_candidates(cell: Cell, values: list|set):
+def remove_from_candidates(cell: Cell, values: list | set):
     cell.candidates = [c for c in cell.candidates if c not in values]
 
 
@@ -105,9 +105,94 @@ def naked_pairs_grid(grid: Grid, skip_cells_with_index: list = None) -> list:
         new_naked_pairs = naked_pairs(col, skip_cells_with_index)
         naked_pair_cell_indexes += new_naked_pairs
         skip_cells_with_index += new_naked_pairs
-    
+
     for box in grid.boxes:
         new_naked_pairs = naked_pairs(box, skip_cells_with_index)
         naked_pair_cell_indexes += new_naked_pairs
 
     return naked_pair_cell_indexes
+
+
+def naked_triplet(group: GroupModel, skip_cells_with_index=None) -> list:
+    """
+    Checks for naked triplets in a group.
+    Removes all three candidates from all other cells in the group.
+    Returns the array of cell indexes for the naked triplets.
+    see https://www.learn-sudoku.com/naked-triplets.html
+    """
+
+    if skip_cells_with_index is None:
+        skip_cells_with_index = []
+    else:
+        # we don't want to change to original list.
+        skip_cells_with_index = skip_cells_with_index.copy()
+
+    naked_triplet_cell_indexes = []
+
+    # iterate over all 3-pack-combinations of cells in this group
+    for c1, c2, c3 in ((c1, c2, c3) for c1 in range(9) for c2 in range(c1 + 1, 9) for c3 in range(c2 + 1, 9)):
+        cell1 = group[c1]
+        cell2 = group[c2]
+        cell3 = group[c3]
+
+        if cell1.index in skip_cells_with_index \
+                or cell2.index in skip_cells_with_index \
+                or cell3.index in skip_cells_with_index:
+            continue
+
+        if not(2 <= len(cell1.candidates) <= 3
+                and 2 <= len(cell2.candidates) <= 3
+                and 2 <= len(cell3.candidates) <= 3):
+            # no potential triplet
+            continue
+
+        candidates1 = set(cell1.candidates)
+        candidates2 = set(cell2.candidates)
+        candidates3 = set(cell3.candidates)
+
+        # one of the values of a cells candidate must be found in each of the other cell candidates
+        if exists_triplet_match(candidates1, candidates2, candidates3):
+            naked_triplet_cell_indexes.append(cell1.index)
+            naked_triplet_cell_indexes.append(cell2.index)
+            naked_triplet_cell_indexes.append(cell3.index)
+
+            # remove triplets from all other cells in group
+            cleanup_values = set(cell1.candidates + cell2.candidates + cell3.candidates)
+            for cleanup_index in (c for c in range(9) if c not in [c1, c2, c3]):
+                cleanup_cell = group[cleanup_index]
+                remove_from_candidates(cleanup_cell, cleanup_values)
+
+    return naked_triplet_cell_indexes
+
+
+def exists_triplet_match(a: list[int] | set[int], b: list[int] | set[int], c: list[int] | set[int]) -> bool:
+    """Checks whether there is a `triplet` in terms of 'Sudoku-Naked-Triplets' in the 3 given lists of values."""
+
+    triplet = set(list(a) + list(b) + list(c))
+    return len(triplet) == 3
+
+
+def naked_triplets_grid(grid: Grid, skip_cells_with_index: list = None) -> list:
+    if skip_cells_with_index is None:
+        skip_cells_with_index = []
+    else:
+        # we don't want to change the original list
+        skip_cells_with_index = skip_cells_with_index.copy()
+
+    naked_triplets_cell_indexes = []
+
+    for row in grid.rows:
+        new_naked_triplets = naked_pairs(row, skip_cells_with_index)
+        naked_triplets_cell_indexes += new_naked_triplets
+        skip_cells_with_index += new_naked_triplets
+
+    for col in grid.columns:
+        new_naked_triplets = naked_pairs(col, skip_cells_with_index)
+        naked_triplets_cell_indexes += new_naked_triplets
+        skip_cells_with_index += new_naked_triplets
+
+    for box in grid.boxes:
+        new_naked_triplets = naked_pairs(box, skip_cells_with_index)
+        naked_triplets_cell_indexes += new_naked_triplets
+
+    return naked_triplets_cell_indexes
--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -1,5 +1,6 @@
 import pytest
-from sudoku_solver_py.algorithms import find_single, reduce_candidates_by_group_constraints, naked_pairs, remove_from_candidates
+from sudoku_solver_py.algorithms import find_single, reduce_candidates_by_group_constraints, naked_pairs, \
+    remove_from_candidates, naked_triplet, exists_triplet_match
 
 from sudoku_solver_py.models import Box, Cell, Column, Grid, Row, GroupModel
 
@@ -126,10 +127,63 @@ def test_naked_pair_skip_cells(group_type: type, cell_factory):
         group_obj.cells.append(cell_factory(value=0, index=ci))
 
     # naked pair - a pair of candidates in two cells of the group
-    group_obj[2].candidates = [2,3]
-    group_obj[5].candidates = [3,2]
+    group_obj[2].candidates = [2, 3]
+    group_obj[5].candidates = [3, 2]
 
     # act
-    naked_pair_indexes = naked_pairs(group_obj, [2,5])
+    naked_pair_indexes = naked_pairs(group_obj, [2, 5])
     # assert
     assert not naked_pair_indexes, 'Naked pair should have been skipped'
+
+
+@pytest.mark.parametrize("init_values,init_candidates,expected_candidates,expected_naked_pair_indexes", [
+    # examples see https://www.learn-sudoku.com/naked-triplets.html
+    (  # true triplets
+            [0, 0, 7, 2, 3, 4, 0, 0, 0],
+            [(1, 5, 9), (1, 5, 9), (), (), (), (), (1, 5, 9), (1, 6, 8, 9), (5, 6, 8, 9)],
+            [(1, 5, 9), (1, 5, 9), (), (), (), (), (1, 5, 9), (6, 8), (6, 8)],
+            [0, 1, 6],
+    ),
+    (  # virtual triplets
+            [0, 2, 0, 0, 0, 0, 0, 0, 0],
+            [(5, 6), (), (1, 4, 8, 9), (4, 7), (8, 6), (1, 6, 7), (3, 4, 7, 9), (3, 4, 5, 7, 9), (5, 8)],
+            [(5, 6), (), (1, 4, 9), (4, 7), (8, 6), (1, 7), (3, 4, 7, 9), (3, 4, 7, 9), (5, 8)],
+            [0, 4, 8],
+    ),
+
+])
+@pytest.mark.parametrize("group_type", [Row, Column, Box])
+def test_naked_triplet(group_type: type, init_values, init_candidates, expected_candidates, expected_naked_pair_indexes,
+                       cell_factory):
+    # arrange
+    group_obj: GroupModel = group_type(index=0)
+
+    for init_value in init_values:
+        group_obj.cells.append(cell_factory(value=init_value))
+
+    for i in range(9):
+        group_obj[i].candidates = list(init_candidates[i])
+
+    # act
+    naked_triplet_indexes = naked_triplet(group_obj)
+    # assert
+    assert naked_triplet_indexes
+
+    for r in range(9):
+        assert set(group_obj[r].candidates) == set(expected_candidates[r])
+
+    assert set(naked_triplet_indexes) == set(expected_naked_pair_indexes)
+
+
+@pytest.mark.parametrize('a,b,c,match_found', [
+    ([1, 5], [5, 9], [1, 9], True),
+    ([1, 5], [5, 8], [1, 9], False),
+    ([1, 5, 9], [1, 5, 9], [1, 5, 9], True),
+    ([1, 5, 9], [1, 5, 9], [1, 5, 8], False),
+    ([1, 5, 9], [1, 5], [1, 9], True),
+    ([1, 5, 9], [1, 5], [5, 9], True),
+    ([5, 9], [1, 5, 9], [1, 9], True),
+    ([5, 9], [1, 5, 9], [1, 8], False),
+])
+def test_exists_triplet_match(a: list[int], b: list[int], c: list[int], match_found: bool):
+    assert exists_triplet_match(a, b, c) == match_found
